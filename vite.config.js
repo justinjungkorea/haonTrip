1552063a1b9ee6839abeb5baa6fc06acbe7b3d2f
justin-dongwook-jung <dongwook.justin.jung@gmail.com>
2025-09-28 06:59:52 +0900

    Added webapp

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,42 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import fs from 'fs'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
+import fs from "fs";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "여행 일정",
+        short_name: "HaonTrip",
+        start_url: ".",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#3b82f6",
+        icons: [
+          {
+            src: "/favicon-192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/favicon-512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
   server: {
     https: {
-      key: fs.readFileSync('./localhost-key.pem'),
-      cert: fs.readFileSync('./localhost.pem'),
+      key: fs.readFileSync("./localhost-key.pem"),
+      cert: fs.readFileSync("./localhost.pem"),
     },
     host: true,
     port: 5173,
   },
-  base:"/"
-})
+  base: "/", // 커스텀 도메인 쓰니까 "/" 유지
+});
